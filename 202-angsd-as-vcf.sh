d59acd346bf7f7fa41506f5720028ccc54e94f81
MacCampbell <campbellmatthewa@gmail.com>
2019-12-16 15:39:38 -0900

    removed "-anc" from doVcf

--- 202-angsd-as-vcf.sh
+++ 202-angsd-as-vcf.sh
@@ -31,6 +31,6 @@ module load benchmarks
 # The main job executable to run: note the use of srun before it
 srun angsd -P 24 sites index outputs/200/200-ranges.tsv
 
-srun angsd -P 24 -b bamlists/test4.bamlist -minInd 43 -anc genome/omyV6Chr.fasta -out outputs/100/test4-maf05p -minMaf 0.05 -minMapQ 30 -minQ 20 -GL 1 -doMajorMinor 1 -doMaf 1 -SNP_pval 1e-6 -doGeno 4 -doPost 1 -postCutoff 0.95 -sites outputs/200/200-ranges.tsv > outputs/200/202.05.out 2> outputs/200/202.05.err
+srun angsd -P 24 -b bamlists/test4.bamlist -minInd 43  -out outputs/200/test4-maf05p-sites -minMaf 0.05 -minMapQ 30 -minQ 20 -GL 1 -doMajorMinor 1 -doMaf 1 -SNP_pval 1e-6 -doGeno 4 -doPost 1 -postCutoff 0.95 -sites outputs/200/200-ranges.tsv > outputs/200/202.05.out 2> outputs/200/202.05.err
 
-srun angsd -P 24 -b bamlists/test5.bamlist -minInd 18 -anc genome/omyV6Chr.fasta -out outputs/100/test5-maf05p -minMaf 0.05 -minMapQ 30 -minQ 20 -GL 1 -doMajorMinor 1 -doMaf 1 -SNP_pval 1e-6 -doGeno 4 -doPost 1 -postCutoff 0.95 -sites outputs/200/200-ranges.tsv > outputs/200/202.05.out 2> outputs/200/202.05.err
+srun angsd -P 24 -b bamlists/test5.bamlist -minInd 18  -out outputs/200/test5-maf05p-sites -minMaf 0.05 -minMapQ 30 -minQ 20 -GL 1 -doMajorMinor 1 -doMaf 1 -SNP_pval 1e-6 -doGeno 4 -doPost 1 -postCutoff 0.95 -sites outputs/200/200-ranges.tsv > outputs/200/202.05.out 2> outputs/200/202.05.err
