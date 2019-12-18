27f0b5d27f12840ce545556ec91ea6503067bcd3
MacCampbell <campbellmatthewa@gmail.com>
2019-12-18 10:06:13 -0900

    Added omy05 PCAs and individual gene trees.

--- 200-identify-multiSNPs.R
+++ 200-identify-multiSNPs.R
@@ -12,7 +12,12 @@ library(tidyverse)
 #cds<-filter(gff, X3=="CDS") %>% select(X1, X4,X5) %>% rename("Chrom"=X1, "Start"=X4, "End"=X5)
 
 genos<-read_tsv(file="outputs/100/test5-maf05p.geno.gz",  col_names = FALSE)  %>% 
-  rename("Chrom"=X1, "Site"=X2)
+  rename("Chrom"=X1, "Site"=X2) %>% mutate(Coord=paste(Chrom,Site, sep="-"))
+#Remove inversion zones
+omy05<-genos %>% filter(Chrom == "omy05" &  Site >= 27001895 & Site <= 81791946) %>% mutate(Coord=paste(Chrom,Site,sep="-")) %>% select(Coord)
+omy20<-genos %>% filter(Chrom == "omy20" &  Site >= 5446054  & Site <= 18985808) %>% mutate(Coord=paste(Chrom,Site,sep="-")) %>% select(Coord)
+genos<-genos %>% filter(!Coord %in% omy05$Coord) %>% filter(!Coord %in% omy20$Coord) %>% select(-Coord)
+
 
 #Identifying RAD Loci
 truncs <- genos %>% select(Chrom, Site) %>% group_by(Chrom) %>% mutate(Next=lead(Site,1)) %>%
@@ -29,6 +34,6 @@ truncs2 <- truncs %>% filter(SnpDiff < 600) %>% group_by(Chrom, Trunc1, Trunc2) 
 # We can generate sequences now.
 # Randomly sampling 100
 # But, we still need to subsitute the proper variants into the resulting sequences....
-loci<-sample_n(truncs2, 100) %>% arrange(Locus)
+loci<-sample_n(truncs2, 1000) %>% arrange(Locus)
 write_tsv(loci, path="outputs/200/200-truncs2.tsv")
 write_tsv(select(loci, Chrom, Left, Right), col_names = FALSE, path="outputs/200/200-ranges.tsv")
